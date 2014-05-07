699a0a2593fcfdcf17c1cfdce96e3a41bd9a52ec
githubUser919 <duk444@gmail.com>
2014-05-07 15:52:51 +0100

    +OWIN Startup class

---
+++ SecurityPipeline/SecurityPipeline/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SecurityPipeline.Startup))]
+
+namespace SecurityPipeline
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var configuration = new HttpConfiguration();
+            configuration.Routes.MapHttpRoute(
+                "default",
+                "api/{controller}");
+
+            app.UseWebApi(configuration);
+        }
+    }
+}
