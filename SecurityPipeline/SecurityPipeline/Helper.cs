f1cbf5e9c85c16f3ed05251ab4820518b3f098eb
githubUser919 <duk444@gmail.com>
2014-05-07 15:29:17 +0100

    +HttpModule and Helper class for debug messages

---
+++ SecurityPipeline/SecurityPipeline/Helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace SecurityPipeline
+{
+    internal class Helper
+    {
+        public static void Write(string stage, IPrincipal principal)
+        {
+            Debug.WriteLine("---------- " + stage + " ------------");
+            if (principal == null || principal.Identity == null || !principal.Identity.IsAuthenticated)
+            {
+                Debug.WriteLine("anonymous user");
+            }
+            else
+            {
+                Debug.WriteLine("User: " + principal.Identity.Name);
+            }
+
+            Debug.WriteLine("\n");
+        }
+    }
+}
---
+++ SecurityPipeline/SecurityPipeline/Pipeline/HttpModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SecurityPipeline
+{
+    public class HttpModule : IHttpModule
+    {
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += context_BeginRequest;
+        }
+
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            Helper.Write("HttpModule", HttpContext.Current.User);
+        }
+
+        public void Dispose() { }
+    }
+}
