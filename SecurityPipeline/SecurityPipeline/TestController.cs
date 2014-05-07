f43cd2d6f31cc97ff8617690b3414740d501a43a
githubUser919 <duk444@gmail.com>
2014-05-07 15:43:44 +0100

    Added WebApi.Core, created TestController

---
+++ SecurityPipeline/SecurityPipeline/TestController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SecurityPipeline
+{
+    public class TestController : ApiController
+    {
+        public IHttpActionResult Get()
+        {
+            Helper.Write("Controller", User);
+
+            return Ok();
+        }
+    }
+}
