fc5ea2f97b73a38a5d67a1a06e050277e2183d42
githubUser919 <duk444@gmail.com>
2014-05-12 12:56:39 +0100

    Client console app consumes api, passing Win credentials

---
+++ WindowsAuthentication/Client/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var handler = new HttpClientHandler()
+            {
+                UseDefaultCredentials = true
+            };
+
+            var client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri("https://localhost:44303/api/")
+            };
+
+            var response = client.GetAsync("identity").Result;
+
+            response.EnsureSuccessStatusCode();
+
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine(content);
+            Console.ReadKey();
+        }
+    }
+}
